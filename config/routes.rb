163746fb08ac8460b254f1cee226a34a2460b8e4
along2000 <alonglongtimeago0130@gmail.com>
2023-03-09 23:00:48 +0900

    sauna#indexの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  #root to: "messages#index"
+  root to: "saunas#index"
+  resources :saunas, only: :index
   resources :users, only: [:edit, :update]
 end
