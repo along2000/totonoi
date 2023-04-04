ac412da19e3ea65e8a333f210b0ec8afd124b992
along2000 <alonglongtimeago0130@gmail.com>
2023-04-04 23:42:58 +0900

    indexにサウナ表示機能を実装

--- app/controllers/saunas_controller.rb
+++ app/controllers/saunas_controller.rb
@@ -9,8 +9,18 @@ class SaunasController < ApplicationController
   end
 
   def create
+    @sauna = Sauna.new(sauna_params)
+    if @sauna.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 private
+    def sauna_params
+      params.require(:sauna).permit(:image, :name, :text, :price, :temperature, :humidity, :cool_bath_temperature, :category_id, :postcode, :prefecture_id,
+        :city, :block, :building, :phone_number).merge(user_id: current_user.id)
+    end
 
 end
