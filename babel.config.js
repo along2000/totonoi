6cfa6733f83412ad16c421cc47edd576d48ea1b6
along2000 <alonglongtimeago0130@gmail.com>
2023-05-08 23:20:59 +0900

    デプロイ

--- babel.config.js
+++ babel.config.js
@@ -38,7 +38,7 @@ module.exports = function(api) {
     ].filter(Boolean),
     plugins: [
       'babel-plugin-macros',
-      '@babel/plugin-proposal-private-methods', 
+
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
